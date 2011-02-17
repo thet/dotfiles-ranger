da8596c8e042cd8258bf0acdd7980a9c9f7cb6cf
Johannes Raggam <raggam-nl@adm.at>
2011-02-17 19:16:52 +0100

    ranger config imported

---
+++ apps.py
@@ -0,0 +1,28 @@
+from ranger.defaults.apps import CustomApplications as DefaultApps
+from ranger.api.apps import tup
+
+class CustomApplications(DefaultApps):
+
+    def app_mplayer_playlist(self, c):
+        return tup('mplayer', '-playlist', *c)
+
+    def app_default(self, c):
+        f = c.file
+
+        if f.extension is not None:
+            if f.extension in ('m3u', 'pls'):
+                return self.app_mplayer_playlist(c)
+            if f.extension in ('pdf', ):
+                c.flags += 'd'
+                return self.either(c, 'okular', 'xpdf', 'evince', 'zathura', 'apvlv')
+            if f.extension in ('torrent', ):
+                c.flags += 'd'
+                return self.either(c, 'deluge')
+            if f.extension in ('psd', 'xcf', ):
+                c.flags += 'd'
+                return self.either(c, 'gimp')
+
+        if f.image:
+            return self.either(c, 'mirage', 'eog', 'feh')
+
+        return DefaultApps.app_default(self, c)
