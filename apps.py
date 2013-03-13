44d200076cd8b7e80770b567c31d75b8ee48c38c
Johannes Raggam <raggam-nl@adm.at>
2013-03-13 03:42:27 +0100

    app conf. use vlc instead of mplayer

--- apps.py
+++ apps.py
@@ -5,6 +5,11 @@ from ranger.api.apps import tup
 class CustomApplications(DefaultApps):
 
     def app_mplayer_playlist(self, c):
+        # create temporary file and play it as playlist
+        import pdb; pdb.set_trace()
+        return tup('mplayer', '-cache', '1024', '-cache-min', '10', '-playlist', *c)
+
+    def app_mplayer_playlist_nocache(self, c):
         return tup('mplayer', '-cache', '1024', '-cache-min', '10', '-playlist', *c)
         #return tup('mplayer', '-playlist', *c)
 
@@ -18,11 +23,17 @@ class CustomApplications(DefaultApps):
             raise SystemExit()
 
         if f.extension is not None:
+            #if f.extension in ('mp3', 'ogg', 'flac', 'wav'):
+            #    return self.app_mplayer_playlist()
             if f.extension in ('m3u', 'pls'):
-                return self.app_mplayer_playlist(c)
+                return self.either(c, 'vlc')
+                #return self.app_mplayer_playlist_nocache(c)
             if f.extension in ('pdf', ):
                 c.flags += 'd'
-                return self.either(c, 'okular', 'xpdf', 'evince', 'zathura', 'apvlv')
+                return self.either(c, 'evince', 'zathura', 'okular', 'xpdf', 'apvlv')
+            if f.extension in ('chm', ):
+                c.flags += 'd'
+                return self.either(c, 'chmsee', 'xchm')
             if f.extension in ('torrent', ):
                 c.flags += 'd'
                 return self.either(c, 'deluge')
